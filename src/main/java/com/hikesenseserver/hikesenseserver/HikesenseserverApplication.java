9fa9d8571a54f0b0e751167b54ee2cfdc5ae65e3
David Hankin <d_hankin@outlook.com>
2024-10-15 13:49:56 +0200

    first commit

---
+++ src/main/java/com/hikesenseserver/hikesenseserver/HikesenseserverApplication.java
@@ -0,0 +1,13 @@
+package com.hikesenseserver.hikesenseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikesenseserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikesenseserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hikesenseserver/hikesenseserver/HikesenseserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikesenseserver.hikesenseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikesenseserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
